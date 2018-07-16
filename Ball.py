c10db2eb00f23e5d19f00884aad4764ae116d210
Rafael <rafael@dragapp.com>
2018-07-16 15:49:56 +0900

    Add Goal Event

--- Ball.py
+++ Ball.py
@@ -14,20 +14,27 @@ class Ball:
         self.Y = int(ball_pos[y])
         self.radius = 10
         self.speed = [0 , 0]
+        self.goal = [0 , 0]
         #self.X = 300
         #self.Y = 200
         pygame.draw.circle(self.window, black, (self.X, self.Y), self.radius)
-    def move (self,dir):
+    def move (self):
         self.X = self.X + self.speed[0]
         self.Y = self.Y + self.speed[1]
-        k = 0.8
+        k = 0.9
         self.speed[0] = int(k * self.speed[0])
         self.speed[1] = int(k * self.speed[1])
 
         print(self.speed)
         
         if (self.X > win_width or self.X < 0):
-            self.speed[0] *= -1
+            if (self.Y > 165 and self.Y  < 240):
+                if (self.X < 0):
+                    self.goal = [0 , 1]
+                else:
+                    self.goal = [1 , 0]
+            else: 
+                self.speed[0] *= -1
         if (self.Y > win_height or self.Y < 0):
             self.speed[1] *= -1
         pygame.draw.circle(self.window, black, (self.X, self.Y), self.radius)
--- soccer.py
+++ soccer.py
@@ -7,10 +7,13 @@ from Player import *
         
 pygame.init()
 pygame.font.init()
+myfont = pygame.font.SysFont('Comic Sans MS', 50)
+
 
 window = pygame.display.set_mode((win_width, win_height))
 BackGround = Background('Field.jpg', [0,0])
 
+points = [0 , 0]
 player1 = player(window)
 ball1 = Ball(window)
 
@@ -56,15 +59,32 @@ while True:
     window.blit(BackGround.image, BackGround.rect)
 
     player1.move(playerDir)
-
+    
     if collision(player1.X,player1.Y,player1.radius,ball1.X,ball1.Y,ball1.radius):
         if pressed[pygame.K_SPACE]                     :
-            ball1.speed = [8*ball1.speed[0], 8*ball1.speed[1]]
-        else:
-            ball1.speed = [4*dirX, 4*dirY]
+            dirX = int((ball1.X - player1.X)/5)
+            dirY = int((ball1.Y - player1.Y)/5)
+#            ball1.speed = [8*ball1.speed[0], 8*ball1.speed[1]]
+    
+        ball1.speed = [4*dirX, 4*dirY]
     #else:
      #   ball1.speed = [0,0]
-    ball1.move([0,0])
+    ball1.move()
+    if (ball1.goal != [0 , 0]):
+        points[0] += ball1.goal[0]
+        points[1] += ball1.goal[1]
+        
+        player1 = player(window)
+        ball1 = Ball(window)
+
+    print(pygame.mouse.get_pos())    
+    
+    player1PointsText = myfont.render(str(points[0]), False, (0, 0, 0))
+    player2PointsText = myfont.render(str(points[1]), False, (0, 0, 0))
+    
+    window.blit(player1PointsText,(win_width/2 - win_width/10,0))
+    window.blit(player2PointsText,(win_width/2 + win_width/10 - 25,0))
+
     pygame.display.update()
     
     clock = pygame.time.Clock().tick(60)
